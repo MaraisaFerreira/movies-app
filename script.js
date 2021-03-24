b90538a48652aca7d85363828e829e1366f182ba
maraisaferreira <ferreira.maraisacristina@gmail.com>
2021-03-24 11:54:32 -0300

    final code

--- script.js
+++ script.js
@@ -4,29 +4,70 @@ const IMG_PATH = 'https://image.tmdb.org/t/p/w1280';
 const SEARCH_API =
 	'https://api.themoviedb.org/3/search/movie?api_key=3fd2be6f0c70a2a598f084ddfb75487c&query="';
 
-const form = document.querySelector('#form')
-const search = document.querySelector('#search')
+const form = document.querySelector('#form');
+const search = document.querySelector('#search');
+const main = document.querySelector('#main');
 
-//get movies
-getMovies(API_URL)
+//obt os filmes da API
+getMovies(API_URL);
 
-async function getMovies(url){
-    const res = await fetch(url)
-    const data = await res.json()
+async function getMovies(url) {
+	const res = await fetch(url);
+	const data = await res.json();
 
-    console.log(data.results);
+	showMovies(data.results);
+}
+
+//exibe os filmes na DOM
+function showMovies(movies) {
+	main.innerHTML = '';
+
+	movies.forEach((movie) => {
+		const { title, poster_path, vote_average, overview } = movie;
+
+		const movieElem = document.createElement('div');
+		movieElem.classList.add('movie');
+
+		movieElem.innerHTML = `
+            
+                <img
+					src="${IMG_PATH + poster_path}"
+				/>
+				<div class="movie-info">
+					<h3>${title}</h3>
+					<span class="${getClassByRate(vote_average)}">${vote_average}</span>
+				</div>
+				<div class="overview">
+					<h3>Overview</h3>
+					${overview}
+				</div>
+			
+        `;
+
+		main.appendChild(movieElem);
+	});
+}
+
+function getClassByRate(vote) {
+	if (vote >= 8) {
+		return 'green';
+	} else if (vote >= 5) {
+		return 'orange';
+	} else {
+		return 'red';
+	}
 }
 
 form.addEventListener('submit', (e) => {
-    e.preventDefault()
+	e.preventDefault();
 
-    const searchTerm = search.value
+	const searchTerm = search.value;
 
-    if(searchTerm && searchTerm != ''){
-        getMovies(SEARCH_API + searchTerm)
+	if (searchTerm && searchTerm != '') {
+		getMovies(SEARCH_API + searchTerm);
 
-        search.value = ''
-    } else {
-        window.location.reload()
-    }
-})
+		search.value = '';
+	} else {
+		window.location.reload();
+	}
+});
